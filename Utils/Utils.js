1bad3820e0538f0008aa1e250ef2c02d0b0a3c51
Bhavya TS <bhavya@inapp.com>
2016-05-10 13:53:48 +0530

    Create Utils.js
    This is a common utility javascript class.

---
+++ Utils/Utils.js
@@ -0,0 +1,22 @@
+/**
+* @author Bhavya
+*/
+function Utils () {}
+
+/**
+* Used to say Hello to anyone
+* @param {String} String the hello message to be returned
+* Returns String with a new parameter representing the value
+*/
+Utils.sayHello = function (params) {
+    return "Hello "+params;
+};
+  
+/**
+* Used to say Good Luck to anyone
+* @param {String} String the hello message to be returned
+* Returns String with a new parameter representing the value
+ */
+Utils.sayGoodLuck = function (sel, val) {
+    return "Good Luck!  "+params;
+};
